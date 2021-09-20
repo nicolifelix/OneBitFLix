5b205a4d2896f50976126b648df086f973022009
nicolifelix <nicolifelix@hotmail.com>
2021-09-20 16:46:23 -0300

    :joystick: Controller Update

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -1,10 +1,34 @@
-class Api::V1::FavoritesController < ApplicationController
-  def index
+def index
+  @favorites = current_user.favorites.all
+  render json:
+Api::V1::WatchableSerializer.new(@favorites.map(&:favoritable)).seria
+lized_json
+ end
+ 
+ def create
+  @favorite = Favorite.new(favorite_params)
+  if @favorite.save
+   head :ok
+  else
+   render json: { errors: @favorite.errors.full_messages }, status:
+:unprocessable_entity
   end
-
-  def create
-  end
-
-  def destroy
-  end
-end
+ end
+ 
+ def destroy
+  @favorite.destroy
+  head :ok
+ end
+ 
+ private
+ 
+ def set_favorite
+  @favorite = Favorite.find_by(favoritable_type:
+params[:type].capitalize!, favoritable_id: params[:id], user:
+current_user)
+ end
+ 
+ def favorite_params
+  params.require(:favorite).permit(:favoritable_type,
+:favoritable_id).merge(user: current_user)
+ end
